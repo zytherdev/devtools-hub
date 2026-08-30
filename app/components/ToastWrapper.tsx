64059a8ef25d947aa01d21b6fcb29220847649a6
IlSognatore <mikepascal.delta@gmail.com>
2026-08-29 20:34:28 -0400

    ....

--- app/components/ToastWrapper.tsx
+++ app/components/ToastWrapper.tsx
@@ -1,20 +1,29 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 'use client';
 
 import { ToastProvider } from "@zyther/react-toastify";
 import "@zyther/react-toastify/styles";
 import { getStoredTheme } from "../lib/utils/thm";
+import { useEffect, useState } from "react";
 
 interface ClientToastProps {
   children: React.ReactNode;
 }
 
 export function ClientToast({ children }: ClientToastProps) {
+  const [theme, setTheme] = useState<'light' | 'dark' | 'system'>('system')
+
+  useEffect(() => {
+    const storedTheme = getStoredTheme();
+    setTheme(storedTheme);
+  }, []);
+  
   return (
     <ToastProvider
       defaultPosition="bottom-right"
       defaultDuration={4000}
       maxToasts={5}
-      theme={getStoredTheme as unknown as ("dark" | "light" ) ?? "light"}
+      theme={theme}
     >
       {children}
     </ToastProvider>
